371ec063e38a6219d4cbd353f52092ef3d431f56
govind <govindarajulu90@gmail.com>
2013-05-16 11:29:45 +0530

    add list_add, list_del

--- main.c
+++ main.c
@@ -5,6 +5,16 @@ struct mylist{
 	struct list_head list;
 };
 struct list_head mylist;
+
+void print_list(struct list_head *listhead)
+{
+	struct mylist *mylist = NULL;
+	printk(KERN_INFO"--------------printing-list--------------\n");
+	list_for_each_entry(mylist, listhead, list) {
+		printk(KERN_INFO"%d\n", mylist->data);
+	}
+	printk(KERN_INFO"-----------------------------------------\n");
+}
 static __init int modinit(void)
 {
 	struct mylist a, b, c, d, e;
@@ -26,10 +36,29 @@ static __init int modinit(void)
 	e.data = 5;
         INIT_LIST_HEAD(&e.list);
 
+	print_list(&mylist);
+
 	list_add(&a.list, &mylist);
+	print_list(&mylist);
 	list_add(&b.list, &mylist);
+	print_list(&mylist);
 	list_add(&c.list, &mylist);
+	print_list(&mylist);
 	list_add(&d.list, &mylist);
+	print_list(&mylist);
+	list_add(&e.list, &mylist);
+	print_list(&mylist);
+
+	list_del(&a.list);
+	print_list(&mylist);
+        list_del(&b.list);
+        print_list(&mylist);
+        list_del(&c.list);
+        print_list(&mylist);
+        list_del(&d.list);
+        print_list(&mylist);
+        list_del(&e.list);
+	print_list(&mylist);
 
 	printk(KERN_INFO"Hello World!\n");
 	return 0;
